a658690517672429e828a8416a72342069e872ae
Doom <clement.doumergue@epitech.eu>
2019-11-11 20:04:57 +0100

    Fix the manifest to use defaults

--- editor/emacs.py
+++ editor/emacs.py
@@ -4,7 +4,6 @@
 import stdlib
 from stdlib.template import autotools
 from stdlib.template.configure import configure
-from stdlib.template.make import make
 from stdlib.manifest import manifest
 
 
@@ -16,7 +15,7 @@ from stdlib.manifest import manifest
     ''',
     tags=['emacs', 'editor', 'gnu'],
     maintainer='doom@raven-os.org',
-    licenses=[stdlib.license.License.GPL_V2],
+    licenses=[stdlib.license.License.GPL_V3],
     upstream_url='https://www.gnu.org/software/emacs/',
     kind=stdlib.kind.Kind.EFFECTIVE,
     versions_data=[
@@ -37,21 +36,13 @@ from stdlib.manifest import manifest
 def build(build):
     packages = autotools.build(
         configure=lambda: configure(
-            f'--prefix=/usr',
-            '--localstatedir=/var',
             '--with-gnutls=no'
         ),
-        compile=lambda: make('all'),
-        install=lambda: make('install', f'DESTDIR={build.install_cache}'),
     )
 
     packages['editor/emacs'].drain(
         'usr/lib64/',
         'usr/share/{applications,icons,emacs}'
     )
-
-    # Packages member of `raven-os/essentials` should explicitly state all
-    # of their dependencies, including indirect ones.
-    packages['editor/emacs'].requires('raven-os/corefs')
 
     return packages
