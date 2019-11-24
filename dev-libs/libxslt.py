f8f22811bb6b1d1e35ae8efd8f5c846447e42e65
Dorian Trubelle <dorian.trubelle@epitech.eu>
2019-11-24 18:42:56 +0100

    Add the dev-libs/xslt build manifest (#28)
    * Add the dev-libs/xslt build manifest
    
    * Rename xslt to libxslt and fixed its build manifest
    
    * Add a comment to explain the configure step

---
+++ dev-libs/libxslt.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3.6
+# -*- coding: utf-8 -*-
+
+import stdlib
+from stdlib.manifest import manifest
+from stdlib.template import autotools
+from stdlib.template.configure import configure
+
+
+def xslt_configure():
+    # This increases the recursion limit in libxslt which is needed by some packages for their documentation
+    stdlib.cmd('sed -i s/3000/5000/ libxslt/transform.c doc/xsltproc.{1,xml}')
+    configure()
+
+
+@manifest(
+    name='libxslt',
+    category='dev-libs',
+    description='''
+    XML stylesheet transformation library.
+    ''',
+    tags=['xml2', 'xml', 'xslt', 'libxslt'],
+    maintainer='dorian.trubelle@epitech.eu',
+    licenses=[stdlib.license.License.CUSTOM],
+    upstream_url='http://xmlsoft.org/XSLT/',  # XMLSoft's website does not support HTTPS
+    kind=stdlib.kind.Kind.EFFECTIVE,
+    versions_data=[
+        {
+            'semver': '2.9.10',
+            'fetch': [{
+                'url': 'http://xmlsoft.org/sources/libxslt-1.1.34.tar.gz',  # XMLSoft's website does not support HTTPS
+                'sha256': '98b1bd46d6792925ad2dfe9a87452ea2adebf69dcb9919ffd55bf926a7f93f7f',
+            }],
+        },
+    ],
+    build_dependencies=[
+        'dev-libs/xml2-dev',
+        'sys-libs/zlib-dev',
+        'sys-apps/xz-dev'
+    ]
+)
+def build(build):
+    packages = autotools.build(
+        configure=lambda: xslt_configure(),
+    )
+
+    packages['dev-libs/libxslt'].drain('usr/lib64/xsltConf.sh')
+
+    return packages
