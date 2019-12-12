4e9304d4ad241e7476f3569284dc7ec0631425a8
Dorian Trubelle <dorian.trubelle@epitech.eu>
2019-12-12 09:25:12 +0100

    Add an instructions file, remove the patch

--- sys-apps/polkit/polkit.py
+++ sys-apps/polkit/polkit.py
@@ -10,15 +10,6 @@ from stdlib.patch import patch
 from stdlib.manifest import manifest
 
 
-def patch_polkit():
-    # There should be a dedicated user and group to take control of the polkitd daemon after it's started
-    stdlib.cmd("groupadd -fg 27 polkitd &&                          \
-        useradd -c 'PolicyKit Daemon Owner' -d /etc/polkit-1 -u 27  \
-        -g polkitd -s /bin/false polkitd                            \
-    ")
-    patch("-Np1 -i ./polkit-0.116-fix_elogind_detection-1.patch")
-
-
 @manifest(
     name='polkit',
     category='sys-apps',
@@ -36,10 +27,7 @@ def patch_polkit():
             'fetch': [{
                 'url': 'https://www.freedesktop.org/software/polkit/releases/polkit-0.116.tar.gz',
                 'sha256': '88170c9e711e8db305a12fdb8234fac5706c61969b94e084d0f117d8ec5d34b1',
-            }, {
-                'file': './polkit-0.116-fix_elogind_detection-1.patch'
-            }
-            ],
+            }],
         },
     ],
     build_dependencies=[
@@ -55,8 +43,7 @@ def patch_polkit():
 )
 def build(build):
     packages = autotools.build(
-        patch=patch_polkit,
-        configure=lambda: configure('--with-os-type=LFS',
+        configure=lambda: configure('--with-os-type=Raven-OS',
                                     '--enable-libsystemd-login=yes',
                                     '--disable-man-pages')
     )
@@ -64,5 +51,7 @@ def build(build):
     packages['sys-apps/polkit-dev'].drain('usr/lib64/')
     packages['sys-apps/polkit-dev'].drain('usr/lib/')
     packages['sys-apps/polkit'].drain('usr/share/')
+
+    packages['sys-apps/polkit'].load_instructions('./instructions.sh')
 
     return packages
